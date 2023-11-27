53b5a831c8e424ce0dcb7a6793ea403ae4c1d83b
Maxoverking <avemax7@gmail.com>
2023-11-27 23:11:53 +0100

    deploy test

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import "./App.module.css";
+
+const App: FC = () => {
+  return (
+    <>
+      <h1>renovierungsarbeiten</h1>
+    </>
+  );
+};
+
+export default App;
